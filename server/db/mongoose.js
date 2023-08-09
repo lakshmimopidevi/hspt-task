f7b40817ff140c3c104ae851cca78c7e9bc2578e
lakshmimopidevi <125748752+lakshmimopidevi@users.noreply.github.com>
2023-08-09 13:45:53 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,15 @@
-var mongoose = require ('mongoose');
-
+var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//change the database with yours
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
+// Change the database connection URL with yours
+mongoose.connect('mongodb+srv://lakshmivvsmarolix:84e966Q7utS2FGj3@cluster0.jhv9u6n.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB successfully!');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
 
-module.exports = {mongoose};
+module.exports = { mongoose };
