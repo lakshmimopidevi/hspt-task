1cef64a06d6f294f7175ffab46e88ede74f75c48
lakshmimopidevi <125748752+lakshmimopidevi@users.noreply.github.com>
2023-08-17 15:12:48 +0530

    Update sonar.js

--- sonar.js
+++ sonar.js
@@ -1,9 +1,9 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://35.172.119.106:9000',
+  serverUrl: 'http://52.35.10.185:9000',
   options: {
-    'sonar.login': 'c261e484f8416f82f3ac910f3e2508583d90d51e',
+    'sonar.login': '1bfcd2cef99b3ba84e26dac79f643f179db50f6b ',
     'sonar.sources': './',
     'sonar.exclusions': 'node_modules/**,public/**,views/**,test/**',
     'sonar.projectKey': 'nhs-app',
